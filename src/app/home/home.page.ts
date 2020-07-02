01f225336650e2a4558dcdb4cdbfb632338c9590
Balram Kola <balramkola@gmail.com>
2020-07-02 19:26:30 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Calci} from '@ionic-native/calci/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  value1: string="";
+  value2: string="";
+
+  constructor(private calci: Calci) {
+    //alert("starting");
+  }
+
+  add(){
+    let data = {
+      param1: this.value1,
+      param2: this.value2
+  }
+  this.calci.add(data).then(result => {
+    alert(result);
+  }).catch(err => alert('Error : ' + err));
+
+  }
+
+  sub(){
+    let data = {
+      param1: this.value1,
+      param2: this.value2
+  }
+  this.calci.substract(data).then(result => {
+    alert(result);
+  }).catch(err => alert('Error : ' + err));
+
+  }
 
 }
