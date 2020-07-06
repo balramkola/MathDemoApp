4ab3be7b5963e5b14827bec95d39d0a1d425002d
Balram Kola <balramkola@gmail.com>
2020-07-06 19:19:27 +0530

    Added CountDown Timer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Calci} from '@ionic-native/calci/ngx';
+import { Calci } from '@ionic-native/calci/ngx';
 
 @Component({
   selector: 'app-home',
@@ -8,32 +8,61 @@ import {Calci} from '@ionic-native/calci/ngx';
 })
 export class HomePage {
 
-  value1: string="";
-  value2: string="";
+  PREFIX_LOG: string = "IONIC UI: ";
+
+  value1: string = "";
+  value2: string = "";
+  timerTextView: string = "";
 
   constructor(private calci: Calci) {
     //alert("starting");
   }
 
-  add(){
+  add() {
     let data = {
       param1: this.value1,
       param2: this.value2
-  }
-  this.calci.add(data).then(result => {
-    alert(result);
-  }).catch(err => alert('Error : ' + err));
+    }
+    console.log(this.PREFIX_LOG + 'add method called');
+    this.calci.add(data).then(result => {
+      console.log(this.PREFIX_LOG + 'addition result:'+ result);
+      alert(result);
+    }).catch(err => alert('Error : ' + err));
 
   }
 
-  sub(){
+  sub() {
     let data = {
       param1: this.value1,
       param2: this.value2
+    }
+    console.log(this.PREFIX_LOG + 'substract method called');
+    this.calci.substract(data).then(result => {
+      console.log(this.PREFIX_LOG + 'substraction result:'+ result);
+      alert(result);
+    }).catch(err => alert('Error : ' + err));
+
   }
-  this.calci.substract(data).then(result => {
-    alert(result);
-  }).catch(err => alert('Error : ' + err));
+
+  hookOnTimerEvents() {
+    // let data = {
+    //   param1: this.value1
+    // }
+    console.log(this.PREFIX_LOG + 'hookOnTimerEvents method called');
+    let onTickSubscription = this.calci.onTick().subscribe(() => {
+      console.log(this.PREFIX_LOG + 'received on TICK call from native code');
+      console.log(this.PREFIX_LOG + 'tick sec:' + this.calci.tickSec);
+      this.timerTextView = "Tick: "+ this.calci.tickSec;
+    });
+
+    let onFinishSubscription = this.calci.onFinish().subscribe(() => {
+      console.log(this.PREFIX_LOG + 'received on FINISH event from native code');
+      onTickSubscription.unsubscribe();
+      console.log(this.PREFIX_LOG + 'unsubscribed from onTick events');
+      onFinishSubscription.unsubscribe();
+      console.log(this.PREFIX_LOG + 'unsubscribed from onFinish events');
+      this.timerTextView = "Timer Finished";
+    });
 
   }
 
